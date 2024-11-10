b5531c9c3b0b0794814feeaef81a37ed086e0e0a
Aashish Shrestha <aashishstha34@gmail.com>
2024-11-10 13:56:57 +0530

    API Fetch using node.js

---
+++ data.js
@@ -0,0 +1,40 @@
+const credentials = [
+  {
+    name: "rohan",
+    email: "rohan@example.com"
+  },
+  {
+    name: "anita",
+    email: "anita@example.com"
+  },
+  {
+    name: "deepak",
+    email: "deepak@example.com"
+  },
+  {
+    name: "sunita",
+    email: "sunita@example.com"
+  },
+  {
+    name: "vijay",
+    email: "vijay@example.com"
+  },
+  {
+    name: "meena",
+    email: "meena@example.com"
+  },
+  {
+    name: "suresh",
+    email: "suresh@example.com"
+  },
+  {
+    name: "rekha",
+    email: "rekha@example.com"
+  },
+  {
+    name: "rajesh",
+    email: "rajesh@example.com"
+  }
+];
+
+module.exports = credentials;
